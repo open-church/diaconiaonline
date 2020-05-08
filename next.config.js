94f90e4c84887b2bf426316572c481649e9c6d81
Marcio Sena <marciosena.inf@gmail.com>
2020-05-08 18:17:42 -0300

    chore: criando link de redirecionamento para criação de membros

--- next.config.js
+++ next.config.js
@@ -2,7 +2,8 @@ require('dotenv').config()
 
 const nextConfig = {
   env: {
-    API_URL: process.env.API_URL
+    API_URL: process.env.API_URL,
+    PREFIX_COMMUNITY_URL: process.env.PREFIX_COMMUNITY_URL
   }
 }
 
